1b82c1388d48fdd1c0bc2c5a8fcffe9ba94ad135
ronresnik <69200570+ronresnik@users.noreply.github.com>
2020-08-22 19:55:08 +0300

    added very initial but working eating functionality
    the eating method is not 100% correct but is quite similar

---
+++ client.py
@@ -0,0 +1,17 @@
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread, Lock
+import time
+
+
+class Client:
+    def __init__(self, name):
+
+    def receive_messages(self):
+
+    def send_message(self, msg):
+
+
+    def get_messages(self):
+
+    def disconnect(self):
+        self.send_message("{quit}")
